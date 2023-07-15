50415b8eb0655c6b28f7a913409df17e1f43249e
etsune <me@etsu.dev>
2023-07-15 12:51:41 +0300

    Add pathfinding

--- src/components/Movement.ts
+++ src/components/Movement.ts
@@ -52,26 +52,82 @@ export function getRandomDirection(availableDirections: MovementAction[]): Movem
   return availableDirections[Math.floor(Math.random() * availableDirections.length)];
 }
 
+// Breadth-first Search
+// Can be used for finding tiles in a sight range
 export function basicBfs(pb: PlayBoard, root: Point, depth: number): Point[] {
   const queue: Point[] = [];
   const explored: Point[] = [];
 
-  queue.unshift(root);
+  queue.push(root);
 
   while (queue.length > 0) {
-    const current = queue.pop();
+    const current = queue.pop()!;
 
-    if (isGroundCell(current!.x, current!.y, pb)) {
-      const neighbors = getDirectionsForPoint(current!)
+    if (isGroundCell(current.x, current.y, pb)) {
+      const neighbors = getDirectionsForPoint(current)
         .filter((e) => !explored.some((c) => c.x === e.x && c.y === e.y))
         .filter((e) => getDistance(e, root) <= depth);
 
       queue.unshift(...neighbors);
     }
-    explored.push(current!);
+    explored.push(current);
   }
 
   return explored;
+}
+
+// Pathfinding function based on BFS
+export function findPathBfs(pb: PlayBoard, root: Point, goal: Point): Point[] {
+  // queue stores tiles we need to check
+  // also has info about a previous position
+  const queue: { pos: Point, prev: Point | null }[] = [];
+
+  // explored -- points we already checked
+  const explored: { [key: string]: Point } = {};
+
+  // path restored based on a backtrace from the goal to the root
+  const path: Point[] = [];
+
+  const strGoalPoint = `${goal.x},${goal.y}`;
+  let strCurrentPoint: string;
+
+  queue.push({
+    pos: root,
+    prev: null,
+  });
+
+  // processing queue of unexplored tiles
+  while (queue.length > 0) {
+    const current = queue.pop()!;
+
+    if (isGroundCell(current.pos.x, current.pos.y, pb)) {
+      const neighbors = getDirectionsForPoint(current.pos)
+        .filter((e) => !Object.keys(explored).includes(`${e.x},${e.y}`))
+        .map((e) => ({
+          pos: e, prev: current.pos,
+        }));
+
+      queue.unshift(...neighbors);
+    }
+
+    strCurrentPoint = `${current.pos.x},${current.pos.y}`;
+
+    explored[strCurrentPoint] = current.prev!;
+    if (strCurrentPoint === strGoalPoint) {
+      break;
+    }
+  }
+
+  // restore path
+  strCurrentPoint = strGoalPoint;
+  while (Object.keys(explored).includes(strCurrentPoint) && explored[strCurrentPoint]) {
+    const currentPt = explored[strCurrentPoint];
+
+    path.unshift(currentPt);
+    strCurrentPoint = `${currentPt.x},${currentPt.y}`;
+  }
+
+  return path;
 }
 
 export function isPossibleDirection(actor: Actor, point: Point, playBoard: PlayBoard): boolean {
