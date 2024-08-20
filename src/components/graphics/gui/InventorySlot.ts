a2fa3bda4ee921ddbfe5b6554615fceeac0f9c5d
Bielwenass <bielwenass@protonmail.com>
2024-08-20 17:00:07 +0400

    Fix inventory slot tint

--- src/components/graphics/gui/InventorySlot.ts
+++ src/components/graphics/gui/InventorySlot.ts
@@ -1,23 +1,31 @@
 import { itemFrameTint } from '@data/items/ItemFrameTint';
-import { Sprite, type Point } from 'pixi.js';
+import {
+  Container, Sprite, type Point,
+} from 'pixi.js';
 
 import { getItemIcon, textureUiInventoryBorder } from '../Graphics';
 
 import type { Item } from '@type/Item';
 import type { ItemSprite } from '@type/ItemSprite';
 
-export class InventorySlot extends Sprite {
+export class InventorySlot extends Container {
   public itemSprite: ItemSprite | null = null;
+
+  private itemBorder: Sprite;
 
   public onClick: Function = Function.prototype;
 
   constructor(position: Point, item: Item | null = null) {
     super(textureUiInventoryBorder);
 
-    this.width = 10;
-    this.height = 10;
-    this.tint = 0x666666;
+    const borderSprite = new Sprite(textureUiInventoryBorder);
+
+    borderSprite.width = 10;
+    borderSprite.height = 10;
+    borderSprite.tint = 0x666666;
+
     this.position = position;
+    this.itemBorder = this.addChild(borderSprite);
     this.setItem(item);
   }
 
@@ -30,24 +38,23 @@ export class InventorySlot extends Sprite {
 
     this.itemSprite?.destroy();
     this.itemSprite = null;
-    this.tint = 0x666666;
+    this.itemBorder.tint = 0x666666;
 
     if (newItem) {
       const newItemSprite = new Sprite(getItemIcon(newItem.type)) as ItemSprite;
 
       newItemSprite.x = 1;
       newItemSprite.y = 1;
-
       newItemSprite.item = newItem;
-      this.tint = itemFrameTint[newItem.rarity];
-      newItemSprite.eventMode = 'static';
 
-      newItemSprite.on('click', () => {
+      this.itemBorder.tint = itemFrameTint[newItem.rarity];
+
+      this.eventMode = 'static';
+      this.on('click', () => {
         this.onClick(newItemSprite);
       });
 
-      this.itemSprite = newItemSprite;
-      this.addChild(newItemSprite);
+      this.itemSprite = this.addChild(newItemSprite);
     }
   }
 
